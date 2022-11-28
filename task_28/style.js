e77990c97771c91f0860db817b8c893c971a7a0f
Nazar <nazar_pilipenko@inbox.ru>
2022-11-28 23:11:28 +0200

    task_28

---
+++ task_28/style.js
@@ -0,0 +1,16 @@
+function londonCityHacker(journey) {
+    let sum = 0;
+    for ( let i = 0; i < journey.length; i++) {
+        if ( typeof journey[i] === 'string'){
+            sum += 2.4;
+        } else {
+            sum += 1.5;
+            if ( typeof journey[i + 1] === 'number') i++;
+        }
+
+    }
+
+    return `${sum.toFixed(2)}`;
+}
+
+console.log(londonCityHacker([12, 'Central', 'Circle', 21]));
