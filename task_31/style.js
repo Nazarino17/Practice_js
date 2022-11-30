31b8869403f0cb995dc479c2dd542af4d7c46550
Nazar <nazar_pilipenko@inbox.ru>
2022-11-30 22:01:58 +0200

    task_31

--- task_31/style.js
+++ task_31/style.js
@@ -3,16 +3,48 @@
 // }
 // console.log(filter_list([12,2,'d','1']));
 
-function filter_list(l) {
+// function filter_list(l) {
 
-    let res = [];
+//     let res = [];
 
-    for ( let i = 0; i < l.length; i++){
-        if ( typeof l[i] == 'number') {
-            res.push(l[i]);
+//     for ( let i = 0; i < l.length; i++){
+//         if ( typeof l[i] == 'number') {
+//             res.push(l[i]);
+//         }
+//     }
+//     return res;
+// }
+
+// console.log(filter_list([21,21,'efew','fwe'])); 
+
+
+
+// function solution(nums){
+//     if (nums !== null) return false;
+//     nums = nums.sort(function(a,b){ return a - b;});
+//     return nums;
+// }
+
+// console.log(solution([1, 10, 2, 3, 5]));
+
+let min = function(list){
+    let max = list[0];
+    for ( let i = 0; i < list.length; i++) {
+        if (list[i] < max) {
+            max = list[i];
         }
-    }
-    return res;
+    }   
+    return max;
+}
+let max = function(list){
+    let max = list[0];
+    for ( let i = 0; i < list.length; i++) {
+        if (list[i] > max) {
+            max = list[i];
+        }
+    }   
+    return max;
 }
 
-console.log(filter_list([21,21,'efew','fwe'])); 
+console.log(max([31,4,1,6,888]));
+console.log(min([31,4,1,6,888]));
