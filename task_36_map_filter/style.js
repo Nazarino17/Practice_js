56e470743aa0250359408426e84649e3fffef6f5
Nazar <nazar_pilipenko@inbox.ru>
2022-12-06 22:17:53 +0200

    task_36_map_filter

--- task_36_map_filter/style.js
+++ task_36_map_filter/style.js
@@ -12,13 +12,49 @@
 function isolateIt(arr) {
     let arrN = arr.map((item) => {
         if (item.length % 2 === 0) {
-            return item.slice(0 ,item.length/2) + "|" + item.slice(item.length / 2);
-        } else {
-            return item.slice(0, item.length/2) + "|" + item.slice(item.length / 2 + 1);
+            return item.slice(0, item.length / 2) + "|" + item.slice(item.length / 2);
+        } else if (item.length % 2) {
+            return item.slice(0, item.length / 2) + "|" + item.slice(item.length / 2 + 1);
 
         }
     });
     return arrN;
 }
 
-console.log(isolateIt(["abcd", "efgh"]));
+
+
+// let a = ["", "a", "ab", "aba", "abab", "ababa"];
+// let b = a.filter(x => {
+//     if (x.length < 2) return true;
+//     let y = x.split("").reverse().join("");
+//     return x == y;
+// });
+// console.log(b);
+
+
+
+function count(arr, n) {
+    return arr.filter( x => x == n ).length;
+}
+let a = [1, 1, 1, 2, 2, 3, 4, 4];
+console.log(count(a, 1)); //output: 3
+console.log(count(a, 2));
+
+
+
+function countGrade(scores){
+    let obj = { // создаем обьект куда будет записывать результат
+        S: 0 , A: 0, B: 0, C: 0, D: 0, X:0
+    };
+    scores.filter( x => { // отфильтровуем елементы , и плюсуем их в обьект
+        if ( x == 100) { obj.S++; }
+        if ( x < 100 && x >=90 ) { obj.A++; }
+        if ( x < 90 && x >= 80) { obj.B++; }
+        if ( x < 80 && x >= 60) { obj.C++; }
+        if ( x < 60 && x >= 0) { obj.D++; }
+        if ( x == -1) { obj.X++; }
+    });
+    return obj; 
+}
+
+console.log(countGrade([50,60,70,80,90,100, -1,-1]));
